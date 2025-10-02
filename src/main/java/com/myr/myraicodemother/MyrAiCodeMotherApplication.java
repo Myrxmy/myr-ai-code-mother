d7aa793273b42a0482c69f515ab38cdb103b358b
Myrxmy <1655895179@qq.com>
2025-10-02 17:27:20 +0800

    后端初始化 1.初始化项目 2.添加依赖和配置（HuTool 和 Knife4j 接口文档） 3.提供基础接口（健康检查接口）

---
+++ src/main/java/com/myr/myraicodemother/MyrAiCodeMotherApplication.java
@@ -0,0 +1,13 @@
+package com.myr.myraicodemother;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyrAiCodeMotherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyrAiCodeMotherApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/myr/myraicodemother/MyrAiCodeMotherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myr.myraicodemother;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyrAiCodeMotherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
